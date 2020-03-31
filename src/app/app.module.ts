e99c6c35c58bf246b90d4600f766eaa4f4f65abe
Dieuveil Mbemba <aldetdieuveil15@gmail.com>
2020-03-31 22:22:10 +0100

    etape 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { Routes, RouterModule } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { LoginComponent } from './login/login.component';
+import { FormationComponent } from './formation/formation.component';
+
+const appRoutes:Routes = [
+{path:'inscription',component:InscriptionComponent},
+{path:'',component:AccueilComponent},
+{path:'formation',component:FormationComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InscriptionComponent,
+    AccueilComponent,
+    LoginComponent,
+    FormationComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
